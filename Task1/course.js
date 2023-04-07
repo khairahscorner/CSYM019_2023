c91e1fd978542ae86b845d951aab2c23a13d36c9
Airah Yusuff <airahyusuff@gmail.com>
2023-04-07 11:17:18 +0100

    feat: updates

--- Task1/course.js
+++ Task1/course.js
@@ -1,16 +1,20 @@
 function populateTable() {
   const xhr = new XMLHttpRequest();
+  const messageArea = document.getElementById("message");
 
   if (xhr) {
     xhr.open("GET", "course.json", true);
     xhr.send();
 
+    //arrow function
     xhr.onreadystatechange = () => {
-      //arrow function
       if (xhr.readyState == 4 && xhr.status == 200) {
         const tableElement = document.getElementById("courses");
 
-        tableElement.innerHTML = ""; //reset table view
+        //reset table view and hide error message area if shown
+        tableElement.innerHTML = "";
+        messageArea.innerHTML = "";
+        messageArea.style.display = "none";
 
         //create thead element for table headers
         const headings = document.createElement("thead");
@@ -19,8 +23,8 @@ function populateTable() {
         headings.appendChild(headingsRow);
         tableElement.appendChild(headings);
 
-        //create 6 columns for the table
-        for (let i = 0; i < 6; i++) {
+        //create 7 columns for the table
+        for (let i = 0; i < 7; i++) {
           const column = document.createElement("th");
           headingsRow.appendChild(column);
           column.innerHTML = `header ${i}`;
@@ -33,6 +37,7 @@ function populateTable() {
         headingsRow.childNodes[3].innerHTML = "Level";
         headingsRow.childNodes[4].innerHTML = "Start Dates";
         headingsRow.childNodes[5].innerHTML = "Duration";
+        headingsRow.childNodes[6].innerHTML = "Actions";
 
         //create tbody for the data listing
         const courseList = document.createElement("tbody");
@@ -42,7 +47,7 @@ function populateTable() {
         for (let i = 0; i < results.length; i++) {
           currentRow = document.createElement("tr");
           currentRow.innerHTML += `<td>${i + 1}</td>`;
-          currentRow.innerHTML += `<td><img src="icons/${results[i].courseDetails.icon}" alt="course logo" class="table-icon"/></td>`;
+          currentRow.innerHTML += `<td><img src="icons/${results[i].courseDetails.icon}" alt="course logo" title="${results[i].courseDetails.subject}" class="table-icon"/></td>`;
           currentRow.innerHTML += `<td>${results[i].courseDetails.courseName}</td>`;
           currentRow.innerHTML += `<td>${results[i].keyFacts.level}</td>`;
 
@@ -77,12 +82,20 @@ function populateTable() {
 
           currentRow.innerHTML += `<td class="cell-with-list">${stringToAppendForDuration}</td>`;
 
+          currentRow.innerHTML += `<td><button><a href="${results[i].courseDetails.url}" target="_blank"> View </a></button></td>`;
+
           courseList.appendChild(currentRow);
         }
         tableElement.appendChild(courseList);
       } else {
+        messageArea.style.display = "inline-block";
+        document.getElementById("message").innerHTML = "Could not load table.";
       }
     };
+  } else {
+    messageArea.style.display = "inline-block";
+    document.getElementById("message").innerHTML =
+      "Could not load table. Please try again later";
   }
 
   //after table is first loaded, then the timeout function is attached
