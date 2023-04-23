7047b53bf11314e9c918be2d67190c1516729940
Airah Yusuff <airahyusuff@gmail.com>
2023-04-23 09:59:14 +0100

    feat: final additions for task 1

--- Task1/course.js
+++ Task1/course.js
@@ -1,6 +1,6 @@
 /**
  * This function is the main function to run that populates the table element in the html file with data from the json file.
- * It is used in the function that is responsible for the live updates of the courses.
+ * It is also used in the function that is responsible for the live updates of the courses.
  */
 function populateTable() {
   $.ajax({ // jquery method for ajax requests
@@ -53,11 +53,7 @@ function populateTable() {
     });
 }
 
-/**
- * this function formats the startDates array such that each available start date for the course is shown as different lines within the same td element
- * @param {Array} startDates start dates array
- * @returns string that can be appended to the innerHTML of the current row
- */
+// function to format the startDates array so each start date is shown as different lines within the same td element
 function formatStringForCourseStartDates(startDates) {
   let stringToAppendForDates = ""; //string to be returned containing the formatted start dates
 
@@ -68,11 +64,7 @@ function formatStringForCourseStartDates(startDates) {
   return stringToAppendForDates;  //return string with the final startDates contents
 }
 
-/**
- * this function formats the duration object such that the fullTime/partTime/Foundation course duration are shown as different lines within the same td element
- * @param {object} duration course duration object 
- * @returns string that can be appended to the innerHTML of the current row 
- */
+//function to format the duration such that each course duration is shown as different lines within the same td element
 function formatStringForCourseDuration(duration) {
   let stringToAppendForDuration = ""; //string to be returned containing the formatted duration
 
@@ -103,41 +95,79 @@ function formatStringForCourseDuration(duration) {
   return stringToAppendForDuration; //return string with the final duration contents
 }
 
-function formatStringForUKFees(fees, rate) {
+//function to format the UK fees in the overlay page for each course
+function formatStringForUKFees(fees) {
   let stringToAppendForFees = ""; //string to be returned
-  stringToAppendForFees += `<div>Full Time: ${fees.fullTime * rate}</div>`;
+  stringToAppendForFees += `<div>Full Time: <span class="uk-fulltime">${fees.fullTime}</span></div>`;
+
   //condition to check if the course has a foundation year option
   if (fees.withFoundation) {
-    stringToAppendForFees += `<div>Foundation Year: ${fees.withFoundation * rate}</div>`;
+    stringToAppendForFees += `<div>Foundation Year: <span class="uk-foundation">${fees.withFoundation}</span></div>`;
   }
   //condition to check if part time exists
   if (fees.partTime) {
     stringToAppendForFees += fees.partTime.length ? (
-      `<div>Part Time: ${fees.partTime[0] * rate} (Year 1), ${fees.partTime[1] * rate} (Year 2)</div>`
+      `<div>Part Time: <span class="uk-parttime1">${fees.partTime[0]}</span> (Year 1), <span class="uk-parttime2">${fees.partTime[1]}</span> (Year 2)</div>`
     ) : (
-      `<div>Part Time: ${fees.partTime * rate} per 20 credits</div>`
+      `<div>Part Time: <span class="uk-parttime">${fees.partTime}</span> per 20 credits</div>`
     )
   }
   return stringToAppendForFees; //return string
 }
 
-function formatStringForInternationalFees(fees, rate) {
+//function to format the international fees in the overlay page for each course
+function formatStringForInternationalFees(fees) {
   let stringToAppendForFees = ""; //string to be returned
-  stringToAppendForFees += `<div>Full Time: ${fees.international.fullTime * rate}</div>`;
+  stringToAppendForFees += `<div>Full Time: <span class="intl-fulltime">${fees.international.fullTime}</span></div>`;
 
   //condition to check if part time exists
   if (fees.international.partTime) {
-    stringToAppendForFees += `<div>Part Time: ${fees.international.partTime * rate}</div>`
+    stringToAppendForFees += `<div>Part Time: <span class="intl-parttime">${fees.international.partTime}</span></div>`
   }
   //condition to check if the course has a foundation year option
   if (fees.international.withFoundation) {
-    stringToAppendForFees += `<div>Foundation Year: ${fees.international.withFoundation * rate}</div>`;
+    stringToAppendForFees += `<div>Foundation Year: <span class="intl-foundation">${fees.international.withFoundation}</span></div>`;
   }
   //condition to check if the course has a placement year option
   if (fees.withPlacement) {
-    stringToAppendForFees += `<div>Placement: ${fees.withPlacement * rate}</div>`;
+    stringToAppendForFees += `<div>Placement: <span class="placement">${fees.withPlacement}</span></div>`;
   }
   return stringToAppendForFees; //return string
+}
+
+//function that updates all UK fees in the table when currency is changed
+function updateUKFees(fees, rate) {
+  // Math.ceil rounds the values to the nearest whole number
+  $(".uk-fulltime").text(Math.ceil(fees.fullTime * rate));
+  if (fees.withFoundation) {
+    $(".uk-foundation").text(Math.ceil(fees.withFoundation * rate));
+  }
+  if (fees.partTime) {
+    if (fees.partTime.length) {
+      $(".uk-parttime1").text(Math.ceil(fees.partTime[0] * rate));
+      $(".uk-parttime2").text(Math.ceil(fees.partTime[1] * rate));
+    }
+    else {
+      $(".uk-parttime").text(Math.ceil(fees.partTime * rate));
+    }
+  }
+}
+
+//function that updates all international fees in the table when currency is changed
+function updateInternationalFees(fees, rate) {
+  // Math.ceil rounds the values to the nearest whole number
+  $(".intl-fulltime").text(Math.ceil(fees.international.fullTime * rate));
+  if (fees.international.partTime) {
+    $(".intl-parttime").text(Math.ceil(fees.international.partTime * rate));
+  }
+  //condition to check if the course has a foundation year option
+  if (fees.international.withFoundation) {
+    $(".intl-foundation").text(Math.ceil(fees.international.withFoundation * rate));
+  }
+  //condition to check if the course has a placement year option
+  if (fees.withPlacement) {
+    $(".placement").text(Math.ceil(fees.withPlacement * rate));
+  }
 }
 
 //main jquery function
@@ -146,21 +176,12 @@ $(document).ready(function () {
 
   // function for updating the table at specific intervals
   (function updateTableAtIntervals() {
-    //settimeout method that executes every 5 minutes
-    setTimeout(function () {
+    setTimeout(function () {  //settimeout method that executes every 5 minutes
       console.log("Now updating"); //sample message to show when table is updated
       populateTable(); // the function that populates the table
-
       updateTableAtIntervals(); // the function calls itself here, creating a recursive cycle
     }, 300000);
   })(); //the function is also self-executing since it is invoked via the () and keeps executing from the recursion
-
-  let rate = 1.0;
-  $('#course-content').on('change', '#currency', function () {
-    rate = parseInt($(this).val());
-    console.log(rate);
-
-  });
 
   $('#table-contents').on('click', '.view', function () { //targets all rows in the table and executes the function on click of each
     let selectedCourseDetails = JSON.parse($(this).closest('tr').attr('data-details')); // retrieve the data-course attribute value and parse back to json
@@ -171,7 +192,8 @@ $(document).ready(function () {
     // appends course name and subject area
     details.append(`<h1 class="details-heading">${selectedCourseDetails.courseDetails.courseName} - <span class="subject">${selectedCourseDetails.courseDetails.subject}</span></h1>`)
     details.append(`<div class="divider"></div>`)
-    // appends first section containing "key facts"
+
+    // appends "key facts" heading and select element for currency selection
     details.append(`<h2 class="section-head">
       Key Facts 
       <div id="currency-wrapper">
@@ -182,6 +204,10 @@ $(document).ready(function () {
           <option value="1.24">$</option>
         </select>
       </div></h2>`)
+
+    let rate = parseFloat($("#currency").val()); //get default selected rate (£)
+
+    // appends first section containing "key facts" as a table
     details.append(`<table>
       <thead>
         <tr>
@@ -197,16 +223,17 @@ $(document).ready(function () {
           <td>${selectedCourseDetails.keyFacts.level}</td>
           <td>${formatStringForCourseStartDates(selectedCourseDetails.keyFacts.startDates)}</td>
           <td>${formatStringForCourseDuration(selectedCourseDetails.keyFacts.duration)}</td>
-          <td class="inner">${formatStringForUKFees(selectedCourseDetails.fees.uk, rate)}</td>
-          <td>${formatStringForInternationalFees(selectedCourseDetails.fees, rate)}</td>
+          <td class="inner">${formatStringForUKFees(selectedCourseDetails.fees.uk)}</td>
+          <td>${formatStringForInternationalFees(selectedCourseDetails.fees)}</td>
         </tr>
       </tbody>
     </table>`)
-    // appends second section
+
+    // appends Overview section - summary of course
     details.append(`<h2 class="section-head">Overview</h2>`)
     details.append(`<div class="summary">${selectedCourseDetails.courseDetails.summary}</div>`)
 
-    // appends another section
+    // appends Highlights section - highlights is an array so .each is used to append each of them
     details.append(`<h2 class="section-head">Highlights</h2>`)
     let highlights = $('<ul>');
     $.each(selectedCourseDetails.courseDetails.highlights, function (index, value) {
@@ -214,9 +241,9 @@ $(document).ready(function () {
     });
     details.append(highlights);
 
-    // appends another section
+    // appends Modules section - formatted to show different fields in the modules
     details.append(`<h2 class="section-head">Modules</h2>`)
-    if (selectedCourseDetails.courseDetails.modules.length) { //PG
+    if (selectedCourseDetails.courseDetails.modules.length) { //if course is postgraduate
       details.append("<strong>Stage 1:</strong>")
       let modules = $('<ul>');
       $.each(selectedCourseDetails.courseDetails.modules, function (index, module) {
@@ -224,7 +251,7 @@ $(document).ready(function () {
       });
       details.append(modules);
     }
-    else {
+    else { //if undergraduate
       details.append("<strong>Stage 1:</strong>")
       let modules = $('<ul>');
       $.each(selectedCourseDetails.courseDetails.modules.stage1, function (index, module) {
@@ -247,14 +274,14 @@ $(document).ready(function () {
       details.append(modules);
     }
 
-    // appends another section
+    // appends Entry Requirements section - formatted to show different fields in the Entry Requirements object
     details.append(`<h2 class="section-head">Entry Requirements</h2>`)
-    if (selectedCourseDetails.keyFacts.level == "Postgraduate") {
+    if (selectedCourseDetails.keyFacts.level == "Postgraduate") { //checks if course is postgraduate
       details.append(`<div>${selectedCourseDetails.entryRequirements.summary}</div>`)
       details.append("<div class='sub-head'>English Language Requirements:</div>")
       details.append(`<div>${selectedCourseDetails.entryRequirements.englishReq}</div>`)
     }
-    else {
+    else { //for undergraduate
       details.append("<div class='sub-head'>Standard:</div>")
       let reqs = $('<ul>');
       $.each(selectedCourseDetails.entryRequirements.summary, function (index, req) {
@@ -272,27 +299,36 @@ $(document).ready(function () {
       details.append(`<div>${selectedCourseDetails.entryRequirements.englishReq}</div>`)
     }
 
-    // appends another section
+    // appends FAQs section - FAQs is an array of question/answer objects so .each is used to append each of them
     details.append(`<h2 class="section-head">FAQs</h2>`)
     faqs = $('<div>');
     $.each(selectedCourseDetails.faqs, function (index, qna) {
-      faqs.append(`<div class="question">${index + 1}) ${qna.question} </div>`);
-      faqs.append(`<div class="answer">${qna.answer}`);
+      faqs.append(`<div class="question">${index + 1}) ${qna.question} </div>`); //shows question
+      faqs.append(`<div class="answer">${qna.answer}`); //shows answer
     });
     details.append(faqs);
 
-    // appends another section
+    // appends relatedCourses section - related courses is an array so .each is used to append each of them
     details.append(`<h2 class="section-head">Related Courses</h2>`)
     relatedCourses = $('<ul>');
-      $.each(selectedCourseDetails.relatedCourses, function (index, course) {
-        relatedCourses.append($('<li>').html(course));
-      });
-      details.append(relatedCourses);
+    $.each(selectedCourseDetails.relatedCourses, function (index, course) {
+      relatedCourses.append($('<li>').html(course));
+    });
+    details.append(relatedCourses);
 
-    $('.overlay').fadeIn();
+    //event listener for change in currency
+    $('#course-content').on('change', '#currency', function () {
+      rate = parseFloat($(this).val()); //get the value of the newly selected currency, parseFloat ensures it's a number
+      updateUKFees(selectedCourseDetails.fees.uk, rate); //calls function to convert uk fees with the new selected rate
+      updateInternationalFees(selectedCourseDetails.fees, rate) //calls function to convert international fees with the new selected rate
+    });
+
+    details.append(`<button><a href="${selectedCourseDetails.courseDetails.url}" target="_blank">View More</a></button>`);
+
+    $('.overlay').fadeIn(); // show the overlay page with transition 
   });
 
-  $('#close-btn').click(function () { //on click of close button, closes the further details section
+  $('#close-btn').click(function () { //on click of close button, closes the overlay section
     $('.overlay').fadeOut();
   });
 })
