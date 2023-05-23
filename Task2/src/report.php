7cccf6c83a150d20fb5de1494d866ff03b0877e3
Airah Yusuff <airahyusuff@gmail.com>
2023-05-23 13:35:14 +0100

    feat: report feature

--- Task2/src/report.php
+++ Task2/src/report.php
@@ -3,33 +3,145 @@
 session_start();
 if ($_SESSION["authenticated"] !== true) {
     header("Location: index.php"); // PHP docs
+} else if (empty($_SESSION['coursesToReport'])) { //ensure the array is not somehow empty
+    header("Location: courselist.php");
+    exit();
 } else {
     require('config_db.php'); // include db setup (https://www.geeksforgeeks.org/how-to-include-content-of-a-php-file-into-another-php-file/)
+    require_once('functions.php');
 
+    $coursesToReport = $_SESSION['coursesToReport'];
 }
 
+//ensures the session variable is deleted, in case of page reload
+unset($_SESSION['coursesToReport']); //https://stackoverflow.com/questions/41867077/remove-session-variable-on-page-refresh
+
+function getCourse(PDO $pdo, $id)
+{
+    $stmt = $pdo->prepare('SELECT * FROM courses WHERE id = :id');
+    $values = [
+        'id' => $id
+    ];
+    $stmt->execute($values);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function getCourseModules(PDO $pdo, $course_id)
+{
+    $stmt = $pdo->prepare('SELECT * FROM modules WHERE course_id = :id ORDER BY stage DESC');
+    $values = [
+        'id' => $course_id
+    ];
+    $stmt->execute($values);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Course Report</title>
     <link rel="stylesheet" href="./layout.css">
+    <?php include("imports.html"); ?>
 </head>
+
 <body>
     <?php include("header.html"); ?>
-    <main>
+    <main id="main-report">
         <?php
-			if (isset($error)) {
-				echo "<p class='error'> $error</p>";
-			}
-			?>
-        <h3 class="main-heading">Sample Course Reoprt</h3>
-        <div class="sketch">
-            <img src="./sampleReport.png" alt="Sample Course Report">
+        if (isset($error)) {
+            echo "<p class='error'> $error</p>";
+        }
+        ?>
+
+        <?php foreach ($coursesToReport as $courseId): ?>
+            <?php
+            $course = getCourse($pdo, $courseId);
+            $courseModules = getCourseModules($pdo, $courseId);
+            $start_dates = json_decode($course['start_dates'], true);
+            $created_at = $date = new DateTime($course['created_at']);
+            $updated_at = $date = new DateTime($course['updated_at']);
+            ?>
+            <div class="section-group report">
+                <div class="first-col">
+                    <h3>
+                        <?php echo $course['course_name'] ?>
+                    </h3>
+                    <table id="courses">
+                        <tbody id='table-contents'>
+                            <tr>
+                                <td class="label">Course Name</td>
+                                <td>
+                                    <?php echo $course['course_name'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label">Subject Area</td>
+                                <td>
+                                    <?php echo $course['subject'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label">Level</td>
+                                <td>
+                                    <?php echo $course['level'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label">Start Dates</td>
+                                <td class="cell-with-list">
+                                    <?php echo implode(', ', $start_dates) ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label">Date Added</td>
+                                <td class="cell-with-list">
+                                    <?php echo $created_at->format('d/m/Y') ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label">Last Updated</td>
+                                <td class="cell-with-list">
+                                    <?php echo $updated_at->format('d/m/Y') ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button type="button" class="view-more"
+                        data-details="<?php echo htmlspecialchars(json_encode($course), ENT_QUOTES, 'UTF-8') ?>"
+                        data-modules="<?php echo htmlspecialchars(json_encode($courseModules), ENT_QUOTES, 'UTF-8') ?>">View
+                        More
+                    </button>
+                </div>
+                <div class="second-col">
+                    <h3> Modules Chart</h3>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
+        <?php if (count($coursesToReport) > 1): ?>
+            <div class="section-group">
+                <div>
+                    Here for more than 1 course
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <div class="overlay">
+            <!-- element to show further course details on click of row -->
+            <div class="overlay-content">
+                <div id="close-btn">
+                    <!-- div elements were used to define the close icon  -->
+                    <div id="line1"></div>
+                    <div id="line2"></div>
+                </div>
+                <div id="course-content"></div> <!-- element containing the course details -->
+            </div>
         </div>
     </main>
-    
+
     <footer>&copy; CSYM019 2023</footer>
 </body>
+
 </html>
--- Task2/src/script.js
+++ Task2/src/script.js
@@ -20,6 +20,17 @@ $(document).ready(function () {
     $('#level-select').change(function () {
         showRightFields($(this).val());
     });
+
+    $('#main-report').on('click', '.view-more', function () {
+        //targets all rows in the table and executes the function on click of each
+        let selectedCourseDetails = JSON.parse($(this).closest('button').attr('data-details')); // retrieve the data-course attribute value and parse back to json
+        let selectedCourseModules = JSON.parse($(this).closest('button').attr('data-modules')); // retrieve the data-modules attribute value and parse back to json
+        populateOverlay(selectedCourseDetails, selectedCourseModules);
+    });
+
+    $('#close-btn').click(function () { //on click of close button, closes the overlay section
+        $('.overlay').fadeOut();
+    });
 })
 
 function showRightFields(type) {
@@ -37,4 +48,197 @@ function showRightFields(type) {
         $('#postgraduate-fields').hide();
         $('#general-fields').hide();
     }
+}
+
+// function to format the startDates array so each start date is shown as different lines within the same td element
+function formatStringForCourseStartDates(startDates) {
+    let stringToAppendForDates = ""; //string to be returned containing the formatted start dates
+
+    // iterate through the length of the startDates array
+    for (let j = 0; j < startDates.length; j++) {
+        stringToAppendForDates += `<div>${startDates[j]}</div>`; // create the string for div elements that'd form each line.
+    }
+    return stringToAppendForDates;  //return string with the final startDates contents
+}
+
+//function that updates all UK fees in the table when currency is changed
+function updateFees(fees, rate) {
+    // Math.ceil rounds the values to the nearest whole number
+    $(".uk-fulltime").text(Math.ceil(fees.fullTime * rate));
+    if (fees.withFoundation) {
+        $(".uk-foundation").text(Math.ceil(fees.withFoundation * rate));
+    }
+    if (fees.partTime) {
+        $(".uk-parttime").text(Math.ceil(fees.partTime * rate));
+    }
+    $(".intl-fulltime").text(Math.ceil(fees.international.fullTime * rate));
+    if (fees.international.partTime) {
+        $(".intl-parttime").text(Math.ceil(fees.international.partTime * rate));
+    }
+    //condition to check if the course has a foundation year option
+    if (fees.international.withFoundation) {
+        $(".intl-foundation").text(Math.ceil(fees.international.withFoundation * rate));
+    }
+    //condition to check if the course has a placement year option
+    if (fees.withPlacement) {
+        $(".placement").text(Math.ceil(fees.withPlacement * rate));
+    }
+}
+
+function filterModules(modules, str) {
+    modules.filter((module) => module.stage === str);
+    return modules;
+}
+
+function populateOverlay(selectedCourseDetails, selectedCourseModules) {
+    const details = $("#course-content"); // targets the element for displaying more course details
+    details.html(""); //resets the element for a new view
+
+    // appends course name and subject area
+    details.append(`<h1 class="details-heading">${selectedCourseDetails.course_name} - <span class="subject">${selectedCourseDetails.subject}</span></h1>`)
+    details.append(`<div class="divider"></div>`)
+
+    // appends "key facts" heading and select element for currency selection
+    details.append(`<h2 class="section-head">
+        Key Facts 
+        <div id="currency-wrapper">
+          <label for="currency">Currency:</label>
+          <select name="select-currency" id="currency">
+            <option selected value="1">£</option>
+            <option value="1.12">€</option>
+            <option value="1.24">$</option>
+          </select>
+        </div></h2>`)
+
+    let rate = parseFloat($("#currency").val()); //get default selected rate (£)
+
+    // appends first section containing "key facts" as a table
+    details.append(`<table>
+        <thead>
+          <tr>
+            <th>Level</th>
+            <th>Start Dates</th>
+            <th>Duration (Years)</th>
+            <th>UK Fees (${selectedCourseDetails.fees_year})</th>
+            <th>International Fees (${selectedCourseDetails.fees_year})</th>
+          </tr>
+        </thead>
+        <tbody id="table-contents">
+          <tr>
+          <td>${selectedCourseDetails.level}</td>
+          <td>${formatStringForCourseStartDates(JSON.parse(selectedCourseDetails.start_dates))}</td>
+          <td>
+          <div>Full Time: ${selectedCourseDetails.duration_fulltime}</div>
+          ${selectedCourseDetails.duration_parttime ? `<div>Part Time: ${selectedCourseDetails.duration_parttime}</div>` : ''}
+          ${selectedCourseDetails.duration_foundation ? `<div>With Foundation: ${selectedCourseDetails.duration_foundation}</div>` : ''}
+          ${selectedCourseDetails.duration_placement === 1 ? `<div>Placement Option available</div>` : ''}
+          ${selectedCourseDetails.fees_withplacement === 1 ? `<div>Placement Fee: <span class="placement">${selectedCourseDetails.fees_withplacement}</span></div>` : ''}
+          </td>
+          <td>
+          <div>Full Time: <span class="uk-fulltime">${selectedCourseDetails.fees_uk_fulltime}</span></div>
+          ${selectedCourseDetails.fees_uk_parttime ? `<div>Part Time: <span class="uk-parttime">${selectedCourseDetails.fees_uk_parttime}</span>` : ''}
+          ${selectedCourseDetails.fees_uk_foundation ? `<div>Foundation Year: <span class="uk-foundation">${selectedCourseDetails.fees_uk_foundation}</span></div>` : ''}
+          </td>
+          <td>
+          <div>Full Time: <span class="intl-fulltime">${selectedCourseDetails.fees_intl_fulltime}</span></div>
+          ${selectedCourseDetails.fees_intl_parttime ? `<div>Part Time: <span class="intl-parttime">${selectedCourseDetails.fees_intl_parttime}</span>` : ''}
+          ${selectedCourseDetails.fees_intl_foundation ? `<div>Foundation Year: <span class="intl-foundation">${selectedCourseDetails.fees_intl_foundation}</span></div>` : ''}
+          </td>
+          </tr>
+        </tbody>
+      </table>`)
+
+    // appends Overview section - summary
+    if (selectedCourseDetails.summary) {
+        details.append(`<h2 class="section-head">Overview</h2>`)
+        details.append(`<div class="summary">${selectedCourseDetails.summary}</div>`)
+    }
+
+    // appends Highlights section - highlights is an array so .each is used to append each of them
+    if (selectedCourseDetails.highlights) {
+        details.append(`<h2 class="section-head">Highlights</h2>`)
+        let highlights = $('<ul>');
+        $.each(JSON.parse(selectedCourseDetails.highlights), function (_, value) {
+            highlights.append($('<li>').text(value));
+        });
+        details.append(highlights);
+    };
+
+    // appends Modules section - formatted to show different fields in the modules
+    if (selectedCourseModules.length > 0) {
+        details.append(`<h2 class="section-head">Modules</h2>`)
+        if (selectedCourseDetails.level.toLowerCase() === "postgraduate") { //if course is postgraduate
+            let modules = $('<ul>');
+            $.each(selectedCourseModules, function (_, module) {
+                modules.append($('<li>').html(`${module.title} (${module.module_code}) - ${module.credits} credits: <strong>${module.status}</strong>`));
+            });
+            details.append(modules);
+        }
+        else { //if undergraduate
+            let lastStage = selectedCourseModules[0].stage; //identify last stage
+            let num = lastStage[lastStage.length - 1]; //get number to know max number of stages
+            for (let i = 1; i <= num; i++) {
+                details.append(`<strong>Stage ${i}:</strong>`);
+                let modules = $('<ul>');
+                $.each(filterModules(selectedCourseModules, `stage${i}`), function (_, module) {
+                    modules.append($('<li>').html(`${module.title} (${module.module_code}) - ${module.credits} credits: <strong>${module.status}</strong>`));
+                });
+                details.append(modules);
+            }
+        }
+    }
+
+    // appends Entry Requirements section - formatted to show different fields in the Entry Requirements object
+    if (selectedCourseDetails.req_summary) {
+        details.append(`<h2 class="section-head">Entry Requirements</h2>`)
+        if (selectedCourseDetails.level.toLowerCase() === "undergraduate") { //checks if course is postgraduate
+            details.append("<div class='sub-head'>Standard:</div>")
+        }
+        let reqs = $('<ul>');
+        $.each(JSON.parse(selectedCourseDetails.req_summary), function (_, req) {
+            reqs.append($('<li>').html(req));
+        });
+        details.append(reqs);
+    }
+    if (selectedCourseDetails.req_foundation) {
+        details.append("<div class='sub-head'>Foundation Year:</div>")
+        reqs = $('<ul>');
+        $.each(JSON.parse(selectedCourseDetails.req_foundation), function (_, req) {
+            reqs.append($('<li>').html(req));
+        });
+        details.append(reqs);
+    }
+    if (selectedCourseDetails.english_req) {
+        details.append("<div class='sub-head'>English Language Requirements:</div>")
+        details.append(`<div class='text'>${selectedCourseDetails.english_req}</div>`)
+    }
+
+    // appends relatedCourses section - related courses is an array so .each is used to append each of them
+    if (selectedCourseDetails.related_courses) {
+        details.append(`<h2 class="section-head">Related Courses</h2>`)
+        relatedCourses = $('<ul>');
+        $.each(JSON.parse(selectedCourseDetails.related_courses), function (_, course) {
+            relatedCourses.append($('<li>').html(course));
+        });
+        details.append(relatedCourses);
+    }
+
+    //event listener for change in currency
+    $('#course-content').on('change', '#currency', function () {
+        rate = parseFloat($(this).val()); //get the value of the newly selected currency, parseFloat ensures it's a number
+        fees = {
+            fullTime: selectedCourseDetails.fees_uk_fulltime,
+            partTime: selectedCourseDetails.fees_uk_parttime,
+            withFoundation: selectedCourseDetails.fees_uk_foundation,
+            withPlacement: selectedCourseDetails.fees_withplacement,
+            international: {
+                fullTime: selectedCourseDetails.fees_intl_fulltime,
+                partTime: selectedCourseDetails.fees_intl_parttime,
+                withFoundation: selectedCourseDetails.fees_intl_foundation
+            }
+        }
+        updateFees(fees, rate); //calls function to convert all fees with the new selected rate
+    });
+
+    $('.overlay').fadeIn(); // show the overlay page with transition 
 }
